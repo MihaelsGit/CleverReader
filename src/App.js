174a60e97b0a5c0560803173609cae028252799c
IvanJuricic <ivanjuricic97@gmail.com>
2022-11-10 22:55:09 +0100

    add upload page design

--- src/App.js
+++ src/App.js
@@ -2,15 +2,23 @@ import React from "react";
 import PDFViewer from "./components/PDFViewer";
 import FileUpload from "./components/FileUpload";
 
+import CustomButton from "./components/CustomButton";
+import FileDropzone from "./components/FileDropzone";
+
 import { test } from "./constants/testUrls";
 
 import "./styles/App.css";
+import CustomHeader from "./components/CustomHeader";
 
 function App() {
   return (
     <div className="wrapper">
-      <h1>Clever Reader</h1>
-      <FileUpload />
+      <CustomHeader text="CleverReader" />
+      <div className="dropzone">
+        <FileDropzone />
+        <CustomButton />
+      </div>
+      {/*<FileUpload />*/}
       {/*<PDFViewer url={test} />*/}
     </div>
   );
--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+import "../styles/Button.css";
+
 function CustomButton() {
   return (
-    <div>
+    <div className="button">
       <button type="submit" className="btn btn-outline-primary">
         UPLOAD
       </button>
