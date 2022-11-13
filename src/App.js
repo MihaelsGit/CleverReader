70502bf62580463a50c1cc509513c3fd3bdb3f10
IvanJuricic <ivanjuricic97@gmail.com>
2022-11-13 13:35:08 +0100

    code cleanup

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./styles/App.css";
 import CustomHeader from "./components/CustomHeader";
 import FileUpload from "./components/FileUpload";
 
+import { projectName } from "./constants/strings";
+
 function App() {
   return (
     <div className="wrapper">
-      <CustomHeader text="CleverReader" />
+      <CustomHeader text={projectName} />
       <FileUpload />
     </div>
   );
