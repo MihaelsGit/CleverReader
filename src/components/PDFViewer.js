645ca6c560ef11a7dcb50cda031ce8fac5ffe87c
Mihael Kampić <mihael.kampic@gmail.com>
2022-11-18 12:59:10 +0100

    Fix PDF Scrolling to work with useEffect hook

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -11,94 +11,57 @@ export default function PDFViewer({ fileID }) {
   const [pdfRef, setPdfRef] = useState();
 
   const renderPaper = useCallback (
-    (pdf = pdfRef) => {
-      /*
-
-      var container = document.getElementById("container");
-
-      for(var i = 1; i <= pdf.numPages; i++) {
-        pdf.getPage(i).then(function(page) {
-
-        var viewport = page.getViewport({ scale: 2 });
-        var div = document.createElement("div");
-
-        div.setAttribute("id", "page-" + (page.pageNum + 1));
-        div.setAttribute("style", "position: relative");
-        container.appendChild(div);
-
-        var canvas = document.createElement("canvas");
-        div.appendChild(canvas);
-
-        var context = canvas.getContext('2d');
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-
-        var renderContext = {
-          canvasContext: context,
-          viewport: viewport
-        };
-
-        page.render(renderContext);
+    (pdf = pdfRef, currentPageNumber ) => {
+      if(pdf == null) return
+      if(currentPageNumber <= pdf.numPages) {
+        pdf.getPage(currentPageNumber).then(function(page) {
+          const container = document.getElementById("container");
+      
+          const viewport = page.getViewport({ scale: 2 });
+          const div = document.createElement("div");
+      
+          div.setAttribute("id", "page-" + (page.pageNum + 1));
+          div.setAttribute("style", "position: relative");
+          container.appendChild(div);
+      
+          const canvas = document.createElement("canvas");
+          div.appendChild(canvas);
+      
+          const outputScale = window.devicePixelRatio || 1;
+      
+          canvas.width = Math.floor(viewport.width * outputScale);
+          canvas.height = Math.floor(viewport.height * outputScale);
+          canvas.style.width = Math.floor(viewport.width) + "px";
+          canvas.style.height = Math.floor(viewport.height) + "px";
+      
+          const transform =
+          outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
+      
+          const renderContext = {
+            canvasContext: canvas.getContext("2d"),
+            transform: transform,
+            viewport: viewport
+          };
+      
+          page.render(renderContext);
+          renderPaper(pdf, currentPageNumber + 1);
         });
-      }*/
+      }
     },
     [pdfRef]
   );
 
-function renderRecursively(pdf, currentPageNumber) {
-  if(currentPageNumber <= pdf.numPages) {
-    pdf.getPage(currentPageNumber).then(function(page) {
-      var container = document.getElementById("container");
-  
-      var viewport = page.getViewport({ scale: 2 });
-      var div = document.createElement("div");
-  
-      div.setAttribute("id", "page-" + (page.pageNum + 1));
-      div.setAttribute("style", "position: relative");
-      container.appendChild(div);
-  
-      var canvas = document.createElement("canvas");
-      div.appendChild(canvas);
-  
-      var outputScale = window.devicePixelRatio || 1;
-  
-      canvas.width = Math.floor(viewport.width * outputScale);
-      canvas.height = Math.floor(viewport.height * outputScale);
-      canvas.style.width = Math.floor(viewport.width) + "px";
-      canvas.style.height = Math.floor(viewport.height) + "px";
-  
-      var transform =
-      outputScale !== 1 ? [outputScale, 0, 0, outputScale, 0, 0] : null;
-  
-      var renderContext = {
-        canvasContext: canvas.getContext("2d"),
-        transform: transform,
-        viewport: viewport
-      };
-  
-      page.render(renderContext);
-      renderRecursively(pdf, currentPageNumber + 1);
-    });
-  }
-}
-
-  /*useEffect(() => {
-    renderPaper(pdfRef);
-  }, [pdfRef, renderPaper, fileID]);*/
-
-  // The method useEffect gets called twice!
-  var counter = 0;
+  useEffect(() => {
+    renderPaper(pdfRef, 1);
+  }, [pdfRef, renderPaper, fileID]);
 
   useEffect(() => {
     if (fileID !== null) {
       let path = BASE_URL + fileID;
-      const loadingTask = pdfjsLib.getDocument("https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf");
+      const loadingTask = pdfjsLib.getDocument(path);
       loadingTask.promise.then(
         (loadedPdf) => {
-          counter++;
-          if(counter == 1 ) return;
-          //setPdfRef(loadedPdf);
-          renderRecursively(loadedPdf, 1);
+          setPdfRef(loadedPdf);
         },
         (reason) => {
           console.error(reason);
