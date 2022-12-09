17af5a12d0b66119cbcd7e48afc6b25d6bd5a131
Patrick Niantcho <patrick.niantcho@gmail.com>
2022-12-09 02:45:18 +0100

    Add loading animation

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -1,19 +1,47 @@
 import React, { useEffect, useState } from "react";
-
+import "../styles/error.css";
 import { BASE_URL } from "../constants/path";
 import "../styles/Viewer.css";
+import LoadingAnimation from "./LoadingAnimation"
 
 export default function PDFViewer({ fileID }) {
   const [pdfURL, setPdfURL] = useState("");
+  const [loading, setLoading] = useState(true);
+  var Spinner = require('react-spinkit');
 
   useEffect(() => {
     let path = BASE_URL + fileID;
     setPdfURL(path);
   }, [fileID]);
 
+  const hideSpinner = () => {
+    setLoading(false);
+    };
+
+  const loadingStyle = {
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100vh',
+          }
+
   return (
     <div className="container">
-      <iframe className="iframe" title="pdfviewer" src={pdfURL} />
+      {loading ? (
+        <div style={loadingStyle}>
+            <LoadingAnimation />
+        </div>
+        
+      ) : null}
+      
+    /*<iframe className="iframe" title="pdfViewer" src={pdfURL} onLoad={hideSpinner} />
     </div>
   );
 }
+
+/*style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100vh',
+          }}*/
