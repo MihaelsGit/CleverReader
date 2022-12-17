333100cf5d33cf80caaf9c4377c1e795ed0ac63f
IvanJuricic <ivanjuricic97@gmail.com>
2022-12-17 22:46:54 +0100

    move PDF viewer initialization in separate file

--- src/components/PDFViewer.js
+++ src/components/PDFViewer.js
@@ -3,10 +3,7 @@ import "../styles/error.css";
 import { BASE_URL } from "../constants/path";
 import "../styles/Viewer.css";
 
-import * as pdfjsLib from "../../node_modules/pdfjs-dist/build/pdf";
-import * as pdfjsViewer from "../../node_modules/pdfjs-dist/web/pdf_viewer";
-import pdfjsWorker from "pdfjs-dist/build/pdf.worker.entry";
-import "../../node_modules/pdfjs-dist/web/pdf_viewer.css";
+import { initializeViewer } from "../components/PDFInitialize";
 
 export default function PDFViewer({ fileID, setLoading }) {
   const [pdfURL, setPdfURL] = useState("");
@@ -15,52 +12,7 @@ export default function PDFViewer({ fileID, setLoading }) {
     let path = BASE_URL + fileID;
     setPdfURL(path);
     try {
-      if (!pdfjsLib.getDocument || !pdfjsViewer.PDFViewer) {
-        // eslint-disable-next-line no-alert
-        console.log(
-          "Please build the pdfjs-dist library using\n  `gulp dist-install`"
-        );
-      }
-
-      pdfjsLib.GlobalWorkerOptions.workerSrc = pdfjsWorker;
-
-      const CMAP_URL = "../../node_modules/pdfjs-dist/cmaps/";
-      const CMAP_PACKED = true;
-
-      const ENABLE_XFA = true;
-
-      const container = document.getElementById("viewerContainer");
-
-      const eventBus = new pdfjsViewer.EventBus();
-
-      const pdfLinkService = new pdfjsViewer.PDFLinkService({
-        eventBus,
-      });
-
-      const pdfViewer = new pdfjsViewer.PDFViewer({
-        container,
-        eventBus,
-        linkService: pdfLinkService,
-      });
-      pdfLinkService.setViewer(pdfViewer);
-
-      eventBus.on("pagesinit", function () {
-        pdfViewer.currentScaleValue = "page-width";
-      });
-      const loadingTask = pdfjsLib.getDocument({
-        url: pdfURL,
-        cMapUrl: CMAP_URL,
-        cMapPacked: CMAP_PACKED,
-        enableXfa: ENABLE_XFA,
-      });
-      (async function () {
-        const pdfDocument = await loadingTask.promise;
-
-        pdfViewer.setDocument(pdfDocument);
-
-        pdfLinkService.setDocument(pdfDocument, null);
-      })();
-
+      initializeViewer(pdfURL);
       setLoading(false);
     } catch (err) {
       console.log("Error showing PDF :( => ", err);
